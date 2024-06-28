005bcd15837dc7a9b6a4831089b3a13f3252aa90
gnomiaaa <haylatheodoro07@gmail.com>
2024-06-28 20:51:52 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,28 @@
+ //variáveis da bolinha
+ let xBolinha = 300;
+ let yBolinha = 200;
+ let diametro = 40;
+ let raio = diametro / 2;
+ 
+ //velocidade da bolinha
+ let velocidadexBolinha = 5;
+ let velocidadeyBolinha = 5;
+ let raqueteComprimento = 15;
+ let raqueteAltura = 90;
+ 
+ 
+ //Variaveis de raquete
+ let xRaquete = 5;
+ let yRaquete = 150;
+ 
+ //variaveis de raquete
+ let xRaqueteOponente = 580;
+ let yRaqueteOponente = 150;
+ let velocidadeYOponente;
+ 
+ let colidiu = false;
+ 
+ //placar do jogo
+ let meusPontos = 0;
+ let pontosDoOponente= 0;
+ 
